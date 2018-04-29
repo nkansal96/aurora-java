5df6319b18c169ab4a655ba22bd5e33f13ae08f1
James Vaughan <james@jamesbvaughan.com>
2018-04-28 17:53:35 -0700

    started on audio recording stuff

--- src/main/java/com/auroraapi/models/Audio.java
+++ src/main/java/com/auroraapi/models/Audio.java
@@ -1,4 +1,20 @@
 package com.auroraapi.models;
 
+import org.jpab.PortAudio;
+import org.jpab.StreamConfiguration.SampleFormat;
+
 public class Audio {
+  static final int BUF_SIZE = Math.pow(2, 10);
+  static final int SILENT_THRESH = Math.pow(2, 11);
+  static final int NUM_CHANNELS = 1;
+  static final int FORMAT = SampleFormat.SIGNED_INTEGER_16;
+  static final int RATE = 16000;
+
+  public RecordResponse record(float length, float silence_len) {
+    PortAudio.initialize();
+
+    Device d = PortAudio.getDevices().get(1);
+
+    return new RecordResponse();
+  }
 }
---
+++ src/main/java/com/auroraapi/models/RecordResponse.java
@@ -0,0 +1,4 @@
+package com.auroraapi.models;
+
+public class RecordResponse {
+}
